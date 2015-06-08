3a97d03e30101f034c5b0a154be1ce35f55a67c5
Tre Murillo <treelzebub@gmail.com>
2015-06-08 00:19:50 -0700

    improve UserUtils; cleanup

--- app/src/main/kotlin/com/treelzebub/umap/util/UserUtils.kt
+++ app/src/main/kotlin/com/treelzebub/umap/util/UserUtils.kt
@@ -1,6 +1,7 @@
 package com.treelzebub.umap.util
 
 import android.content.Context
+import android.util.Log
 import com.treelzebub.umap.api.discogs.constants.USER_FILENAME
 import com.treelzebub.umap.api.discogs.model.User
 import java.io.*
@@ -13,24 +14,37 @@ public class UserUtils() : Serializable {
 
     var user: User? = null
 
-    public fun persist(c: Context) {
-        try {
-            val fileOutStream = c.openFileOutput(USER_FILENAME, Context.MODE_PRIVATE)
-            var objOutStream = ObjectOutputStream(fileOutStream)
-            objOutStream.writeObject(this)
-            objOutStream.close()
-        } catch(e: IOException) {
-            e.printStackTrace()
+    public fun hasUser(): Boolean {
+        return user != null
+    }
+
+    public fun persist(c: Context): Boolean {
+        if (hasUser()) {
+            try {
+                val fileOutStream = c.openFileOutput(USER_FILENAME, Context.MODE_PRIVATE)
+                var objOutStream = ObjectOutputStream(fileOutStream)
+                objOutStream.writeObject(this)
+                objOutStream.close()
+                return true
+            } catch(e: IOException) {
+                e.printStackTrace()
+                return false
+            }
+        } else {
+            Log.d("User Persist Error", "User not set.")
+            return false
         }
     }
 
-    public fun fromFile(c: Context) {
+    public fun fromFile(c: Context): Boolean {
         try {
             val fileInStream = c.openFileInput(USER_FILENAME)
             val objInStream = ObjectInputStream(fileInStream)
             user = objInStream.readObject() as User
+            return true
         } catch (e: IOException) {
             e.printStackTrace()
+            return false
         }
     }
 }
