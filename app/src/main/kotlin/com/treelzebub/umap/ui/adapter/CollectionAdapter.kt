61023b388e7ede324b208603e34e03ec362fd6f3
Tre Murillo <treelzebub@gmail.com>
2015-06-07 18:30:26 -0700

    oh

--- app/src/main/kotlin/com/treelzebub/umap/ui/adapter/CollectionAdapter.kt
+++ app/src/main/kotlin/com/treelzebub/umap/ui/adapter/CollectionAdapter.kt
@@ -1,25 +1,23 @@
 package com.treelzebub.umap.ui.adapter
 
 import android.content.Context
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import com.treelzebub.umap.R
 import com.treelzebub.umap.api.discogs.model.CollectionFolder
 import butterknife.bindView
 
-
 /**
  * Created by Tre Murillo on 6/7/15
- * Copyright(c) 2015 Level, Inc.
  */
 public class CollectionAdapter : RecyclerView.Adapter<CollectionAdapter.ViewHolder>() {
 
     var folders: List<CollectionFolder> = listOf()
-
-    val albumCover: ImageView by bindView(R.id.cardview_album_cover)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
@@ -30,10 +28,14 @@ public class CollectionAdapter : RecyclerView.Adapter<CollectionAdapter.ViewHold
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(parent)
+
     }
 
     private inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val albumCover: ImageView  by bindView(R.id.cardview_album_cover)
+        val artist: TextView       by bindView(R.id.cardview_artist)
+        val title: TextView        by bindView(R.id.cardview_title)
+
 
     }
 }
