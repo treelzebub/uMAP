e8c8189bc1b28963e764f4773d59fc9cbca4ebdb
Tre Murillo <treelzebub@gmail.com>
2015-08-17 20:58:05 -0700

    rm level (c)

--- app/src/main/kotlin/com/treelzebub/umap/auth/AuthState.kt
+++ app/src/main/kotlin/com/treelzebub/umap/auth/AuthState.kt
@@ -5,7 +5,6 @@ import com.treelzebub.umap.util.UserUtils
 
 /**
  * Created by Tre Murillo on 8/17/15
- * Copyright(c) 2015 Level, Inc.
  */
 public object AuthState {
     private synchronized var sAuthRequests = 0
