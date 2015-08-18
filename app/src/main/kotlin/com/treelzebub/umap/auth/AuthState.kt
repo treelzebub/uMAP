201833af374eb1b0cfe7f30f04ecdcba25dc46de
Tre Murillo <treelzebub@gmail.com>
2015-08-17 20:57:33 -0700

    AuthState stub

---
+++ app/src/main/kotlin/com/treelzebub/umap/auth/AuthState.kt
@@ -0,0 +1,19 @@
+package com.treelzebub.umap.auth
+
+import android.content.Context
+import com.treelzebub.umap.util.UserUtils
+
+/**
+ * Created by Tre Murillo on 8/17/15
+ * Copyright(c) 2015 Level, Inc.
+ */
+public object AuthState {
+    private synchronized var sAuthRequests = 0
+
+    private var didLogout = false
+    public fun onDidLogout(c: Context) {
+        if(!didLogout) {
+            didLogout = true
+        }
+    }
+}
