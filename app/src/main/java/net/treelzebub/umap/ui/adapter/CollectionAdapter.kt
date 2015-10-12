d82d4e153e963236b45f5331d8ec27ef0118ff9a
Tre Murillo <treelzebub@gmail.com>
2015-10-11 17:10:57 -0700

    improve adapter

--- app/src/main/java/net/treelzebub/umap/ui/adapter/CollectionAdapter.kt
+++ app/src/main/java/net/treelzebub/umap/ui/adapter/CollectionAdapter.kt
@@ -15,33 +15,28 @@ import net.treelzebub.umap.api.discogs.model.CollectionReleases
 /**
  * Created by Tre Murillo on 6/7/15
  */
-public class CollectionAdapter(collectionReleases: CollectionReleases) : RecyclerView.Adapter<CollectionAdapter.ViewHolder>() {
-
-    var context: Context? = null
-    var collectionReleases = collectionReleases
+public class CollectionAdapter(val c: Context, val releases: CollectionReleases) : RecyclerView.Adapter<CollectionAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, i: Int): ViewHolder {
-        context = parent.context
-        val v = LayoutInflater.from(context).inflate(R.layout.collection_card, parent, false)
-        return ViewHolder(v)
+        return ViewHolder(LayoutInflater.from(c).inflate(R.layout.collection_card, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
-        var albumCoverUrl = collectionReleases.releases!!.get(i).basic_information!!.thumb
-        if (albumCoverUrl == "") {
-            albumCoverUrl = "http://treelzebub.net/img/no-cover.png"
+        var albumCoverUrl = releases.releases.get(i).basic_information.thumb
+        if (albumCoverUrl.isNullOrEmpty()) {
+            albumCoverUrl = "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg" // "http://treelzebub.net/img/no-cover.png"
         }
-        Picasso.with(context)
+        Picasso.with(c)
                 .load(albumCoverUrl)
                 .fit()
                 .centerCrop()
                 .into(holder.albumCover)
-        holder.artist.text = collectionReleases.releases!!.get(i).basic_information!!.artists.first().name
-        holder.title.text = collectionReleases.releases!!.get(i).basic_information!!.title
+        holder.artist.text = releases.releases.get(i).basic_information.artists.first().name
+        holder.title.text = releases.releases.get(i).basic_information.title
     }
 
     override fun getItemCount(): Int {
-        return collectionReleases.releases!!.size()
+        return releases.releases.size()
     }
 
     private inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
