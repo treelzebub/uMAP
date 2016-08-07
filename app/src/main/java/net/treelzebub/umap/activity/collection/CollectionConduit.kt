95560bad8b1fb4e8f678774bf639b12e6af3ed55
Tre Murillo <treelzebub@gmail.com>
2016-08-07 03:53:10 -0700

    post

--- app/src/main/java/net/treelzebub/umap/activity/collection/CollectionConduit.kt
+++ app/src/main/java/net/treelzebub/umap/activity/collection/CollectionConduit.kt
@@ -9,13 +9,11 @@ import net.treelzebub.umap.api.model.CollectionReleases
 import net.treelzebub.umap.auth.user.Users
 
 /**
- * Created by treelzeub on 8/7/16.
- * Copyright(c) 2016 Level, Inc.
+ * Created by Tre Murillo on 8/7/16.
  */
 class CollectionConduit : Conduit<CollectionConduit, CollectionReleases?> {
 
     constructor(a: ObserveAppCompatActivity) : super(a)
-    constructor(f: ObserveSupportFragment) : super(f)
 
     override fun onLoad(args: Bundle?): CollectionReleases? {
         val username = Users.username ?: return null
